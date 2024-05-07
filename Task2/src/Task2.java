1c27dfb52c50e53bb123e8a66dfaee6f8d4c660d
deionnle <render48@yandex.ru>
2024-05-07 23:46:05 +0300

    add Task2.java

---
+++ Task2/src/Task2.java
@@ -0,0 +1,143 @@
+class Heroes {
+    String name; // имя героя
+    int fraction; // 1 - Замок 2 - Оплот, 3 - Башня, 4 - Инферно
+    double level; // уровень
+    double experience; //опыт
+    int magic; // очки заклинаний
+    int morale; // боевой дух
+
+    Heroes(String n, int f, double l, double e, int m, int mor)
+    {
+        this.name = n;
+        this.fraction = f;
+        this.level = l;
+        this.experience = e;
+        this.magic = m;
+        this.morale = mor;
+    }
+
+    // восстанавливает максимальный запас очков заклинаний
+    void magicWell() {
+        this.magic = 50;
+    }
+
+    // Получает опыт
+    void getExperience(double new_experience) {
+        this.experience += new_experience;
+    }
+
+    // Повышает уровень
+    void levelUp() {
+        this.level += this.experience / 10000;
+    }
+
+    // Побеждает в сражении, получает опыт и боевой дух, повышается уровень
+    void win() {
+        this.getExperience(20000);
+        this.morale = 100;
+        this.levelUp();
+    }
+}
+
+class Cities {
+    String name; // название города
+    int fraction; // 1 - Замок 2 - Оплот, 3 - Башня, 4 - Инферно
+    int level; // 1 - Управа, 2 - Форт, 3 - Цитадель, 4 - Замок
+    int money; // казна города
+
+    Cities(String n, int f, int l, int m)
+    {
+        this.name = n;
+        this.fraction = f;
+        this.level = l;
+        this.money = m;
+    }
+
+    // Улучшаем город
+    void levelUp() {
+        if(this.level < 5) {
+            this.level += 1;
+            this.money -= 5000;
+        }
+    }
+
+    // Город захвачен после осады
+    void cityLost() {
+        this.level -= 1;
+    }
+}
+
+class Units {
+    String name;
+    int price;
+    int attack;
+    int protection;
+    int damage;
+    int health;
+
+    Units(String n, int p, int a, int pro, int d, int h) {
+        this.name = n;
+        this.price = p;
+        this.attack = a;
+        this.protection = pro;
+        this.damage = d;
+        this.health = h;
+    }
+
+    // Получает урон в бою
+    void getDamage(int new_damage) {
+        this.health -= (new_damage - this.protection);
+    }
+
+    // хилится героем или палаткой
+    void heal() {
+        if (this.health <= 80) {
+            this.health += 10;
+        }
+    }
+}
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Heroes Gelo = new Heroes("Гело", 2, 1.0, 900.0, 30, 80);
+
+        Gelo.magicWell(); // восстанавливает очки у волшебного колодца
+        Gelo.getExperience(1500); // получает очки опыта
+        Gelo.levelUp(); // уровень растет при получении опыта
+        Gelo.win(); // побеждает в битве
+
+        System.out.println("Характеристики героя: " + Gelo.name);
+        System.out.println("Фракция: " + Gelo.fraction);
+        System.out.println("Уровень: " + Gelo.level);
+        System.out.println("Опыт: " + Gelo.experience);
+        System.out.println("Очки заклинаний: " + Gelo.magic);
+        System.out.println("Боевой дух отряда: " + Gelo.morale);
+        System.out.println();
+
+        Cities Forest = new Cities("Форест", 2, 2, 15000);
+
+        Forest.levelUp(); // улучшаем город
+        Forest.levelUp(); // еще раз до максимума
+        Forest.cityLost(); // враг захватил и разрушил город во время осады
+
+        System.out.println("Характеристики города: " + Forest.name);
+        System.out.println("Фракция: " + Forest.fraction);
+        System.out.println("Уровень: " + Forest.level);
+        System.out.println("Казна: " + Forest.money);
+        System.out.println();
+
+        Units Unicorn = new Units("Единорог", 850, 15, 14, 22, 90);
+
+        Unicorn.getDamage(30); // получает урон в бою
+        Unicorn.getDamage(20);
+        Unicorn.heal();
+
+        System.out.println("Характеристики юнита: " + Unicorn.name);
+        System.out.println("Стоимость: " + Unicorn.price);
+        System.out.println("Атака: " + Unicorn.attack);
+        System.out.println("Защита: " + Unicorn.protection);
+        System.out.println("Урон: " + Unicorn.damage);
+        System.out.println("Здоровье: " + Unicorn.health);
+    }
+}
