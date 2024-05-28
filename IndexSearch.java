c10dbf8d7382d65dfd5ba8859bd35d57f1596532
deionnle <render48@yandex.ru>
2024-05-28 13:38:57 +0300

    add IndexSearch

---
+++ IndexSearch.java
@@ -0,0 +1,24 @@
+import java.util.logging.Logger;
+
+public class IndexSearch {
+    static Logger logger = Logger.getLogger(IndexSearch.class.getName());
+    static int num(int[] array, int a) {
+        logger.info("Функция поиск индекса");
+        assert array.length > 0;
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] == a) {
+                logger.info("Поиск успешно завершен");
+                assert i >= 0 && i < array.length;
+                return i;
+            }
+        }
+        logger.info("Индекс не был найден");
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 4, 30, 2, 36, 8, 11, 7, 94};
+        int x = 94;
+        System.out.println(num(arr, x));
+    }
+}
