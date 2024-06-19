f066fc10434d34464ccbc0bd81a43a73f8f3f27a
deionnle <render48@yandex.ru>
2024-06-19 19:55:10 +0300

    add changes

--- Numbers.java
+++ Numbers.java
@@ -3,40 +3,66 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Numbers {
-    static int getSumSixNumbers (int a, int b, String way) {
+    public static int[] getSumSixNumbers (int a, int b, String way) {
+        int[] sum = new int[2];
+        sum[0] = 0;
+        sum[1] = 0;
+        for (int i = 0; i < 2; i++) {
+            int[] s;
+            if (i == 0) {
+                s = sumSixNumbers(a, way);
+            } else {
+                s = sumSixNumbers(b, way);
+            }
+            if (s[1] != 0) {
+                sum[1] = s[1];
+                return sum;
+            }
+            sum[0] += s[0];
+        }
+        return sum;
+    }
+
+public static int[] sumSixNumbers (int f, String way) {
+    Logger log = Logger.getLogger(Numbers.class.getName());
+        int[] res = new int[2];
+        res[1] = 0;
         int sum = 0;
-        for (int i = 0; i <2; i ++) {
-            File my_fil = new File("" + way + a + ".txt");
-            BufferedReader br = null;
+        BufferedReader br = null;
             try {
+                File my_fil = new File(way + f + ".txt");
                 br = new BufferedReader(new FileReader(my_fil));
                 String st = br.readLine();
                 int count = 0;
                 while (st != null) {
-                    sum += Integer.valueOf(st);
+                    sum += Integer.parseInt(st);
                     st = br.readLine();
                     count++;
                 }
-                a = b;
                 if (count != 3) {
-                    System.err.println("Неверное количество строк");
+                    res[1] = 1;
+                    log.warning("Неверное количество строк");
+                    return res;
                 }
             } catch (IOException e) {
-                System.out.println("Фаил не найден");
+                log.warning ("Файл не найден");
             } catch (NumberFormatException e) {
-                System.out.println("Некорректное значение в строке  " + e);
+                res[1] = 2;
+                log.warning ("Некорректное значение в строке");
             } finally {
                 try {
                     br.close();
                 } catch (IOException e) {
-                    System.out.println("Ошибка " + e);
+                    res[1] = 3;
+                    log.warning("Ошибка чтения файла");
                 }
             }
+            res[0] = sum;
+            return res;
         }
-        return sum;
-    }
 
     public static void main(String[] args) {
 
@@ -45,6 +71,8 @@ public class Numbers {
     int b = rand.nextInt(10) + 1;
     String way = "E:\\Обучение\\exercises\\";
 
-    System.out.println(getSumSixNumbers(a, b, way));
+    int r[] = getSumSixNumbers(a, b, way);
+        System.out.println(r[0]);
+        System.out.println(r[1]);
     }
 }
